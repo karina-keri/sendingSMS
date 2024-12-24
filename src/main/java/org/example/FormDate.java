1ea00cabafdabd502c6715bea1e4994fc3139a15
karina <karina-keri11@mail.ru>
2024-12-24 22:20:22 +0300

    initial commit

---
+++ src/main/java/org/example/FormDate.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class FormDate {
+    public int day;   // поле для дня
+    public int month; // поле для месяца
+    public int year;  // поле для года
+}
---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Post {
+    public String name;      // поле для имени
+    public String surname;   // поле для фамилии
+    public String patronymic; // поле для отчества
+    public String passport;   // поле для паспорта
+    public String phone;      // поле для телефона
+    public boolean subscription; // поле для согласия на подписку
+    public FormDate birthday; // поле для даты рождения
+}
